4aa150a9921343434b9710e6ee88cf6d311e5190
dingli <839074670@qq.com>
2021-01-27 22:55:26 +0800

    --同步controller

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.atguigu.springcloud.service.PaymentService;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ding
@@ -23,6 +26,13 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    /**
+     * spring官方提供的服务发现的restapi
+     * @author ding
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @ApiOperation("新增")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "成功"), @ApiResponse(code = 400, message = "{code:****,message:'失败'}")})
@@ -41,6 +51,29 @@ public class PaymentController {
         log.info("查询");
         System.out.println("server端口：" + serverPort);
         return paymentService.getOneById(id);
+    }
+
+    @ApiOperation("当前微服务中-服务发现")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "成功"), @ApiResponse(code = 400, message = "{code:****,message:'失败'}")})
+    @GetMapping("payments/getServices")
+    public Object getServices() {
+        /**
+         * 获取有多少个service
+         */
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("----" + service);
+
+        }
+        /**
+         * 获取该服务下有多少个实例
+         */
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return discoveryClient;
     }
 
     @ApiOperation("获取")
