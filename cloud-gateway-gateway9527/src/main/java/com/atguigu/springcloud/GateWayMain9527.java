a0ab917a915b8d6c16d2a1d5f36c23c7bfae5fd5
dingli <839074670@qq.com>
2021-03-01 23:31:44 +0800

    --gateway模块以及config模块

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ding
+ * @date 2021.02.28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+        System.out.println("网关启动");
+    }
+}
