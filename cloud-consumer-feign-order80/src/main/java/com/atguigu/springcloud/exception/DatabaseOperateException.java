ba01cf1692a5ea70b950cdb6f6492d818e4f2795
li.ding <li.ding@qq.com>
2021-08-12 16:55:24 +0800

    修改文件名

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/exception/DatabaseOperateException.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/exception/DatabaseOperateException.java
@@ -1,5 +1,8 @@
 package com.atguigu.springcloud.exception;
 
+/**
+ * @author ding.li
+ */
 public class DatabaseOperateException extends RuntimeException {
 
     private static final long serialVersionUID = -123765430L;
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud;
 import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
@@ -11,7 +13,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  * @date 2021.1.1
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
